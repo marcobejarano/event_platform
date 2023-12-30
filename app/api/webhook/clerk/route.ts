027555e5f401b0001347a6298c02172bd2830762
Marco <m_uno_p@hotmail.com>
2023-12-30 17:12:54 -0500

    Fixed index file from database and remove 'use server' from user.actions.ts

--- app/api/webhook/clerk/route.ts
+++ app/api/webhook/clerk/route.ts
@@ -4,7 +4,6 @@ import { WebhookEvent } from '@clerk/nextjs/server'
 import { createUser, deleteUser, updateUser } from '@/lib/actions/user.actions'
 import { clerkClient } from '@clerk/nextjs'
 import { NextResponse } from 'next/server'
-
  
 export async function POST(req: Request) {
  
@@ -54,23 +53,22 @@ export async function POST(req: Request) {
   // Get the ID and type
   const { id } = evt.data;
   const eventType = evt.type;
-
-  if (eventType === "user.created") {
-    const { id, email_addresses, image_url,
-        first_name, last_name, username } = evt.data;
+ 
+  if(eventType === 'user.created') {
+    const { id, email_addresses, image_url, first_name, last_name, username } = evt.data;
 
     const user = {
-        clerkId: id,
-        email: email_addresses[0].email_address,
-        username: username!,
-        firstName: first_name,
-        lastName: last_name,
-        photo: image_url,
+      clerkId: id,
+      email: email_addresses[0].email_address,
+      username: username!,
+      firstName: first_name,
+      lastName: last_name,
+      photo: image_url,
     }
 
     const newUser = await createUser(user);
 
-    if (newUser) {
+    if(newUser) {
       await clerkClient.users.updateUserMetadata(id, {
         publicMetadata: {
           userId: newUser._id
@@ -78,13 +76,12 @@ export async function POST(req: Request) {
       })
     }
 
-    return NextResponse.json({ message: 'OK', user: newUser });
+    return NextResponse.json({ message: 'OK', user: newUser })
   }
 
-  if (eventType === "user.updated") {
-    const { id, image_url, first_name,
-        last_name, username } = evt.data;
-    
+  if (eventType === 'user.updated') {
+    const {id, image_url, first_name, last_name, username } = evt.data
+
     const user = {
       firstName: first_name,
       lastName: last_name,
@@ -92,18 +89,18 @@ export async function POST(req: Request) {
       photo: image_url,
     }
 
-    const updatedUser = await updateUser(id, user);
+    const updatedUser = await updateUser(id, user)
 
-    return NextResponse.json({ message: 'OK', user: updatedUser });
+    return NextResponse.json({ message: 'OK', user: updatedUser })
   }
 
-  if (eventType === "user.deleted") {
-    const { id } = evt.data;
+  if (eventType === 'user.deleted') {
+    const { id } = evt.data
 
-    const deletedUser = await deleteUser(id!);
+    const deletedUser = await deleteUser(id!)
 
-    return NextResponse.json({ message: "OK", user: deletedUser });
+    return NextResponse.json({ message: 'OK', user: deletedUser })
   }
-
+ 
   return new Response('', { status: 200 })
 }
--- lib/actions/user.actions.ts
+++ lib/actions/user.actions.ts
@@ -1,5 +1,3 @@
-'use server'
-
 import { revalidatePath } from 'next/cache'
 
 import { connectToDatabase } from '@/lib/database'
