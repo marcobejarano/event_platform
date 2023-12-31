0530f357c4f71df93acc465eb223aa2b6540819d
Marco <m_uno_p@hotmail.com>
2023-12-31 13:19:49 -0500

    First commit

--- lib/database/models/user.model.ts
+++ lib/database/models/user.model.ts
@@ -4,10 +4,13 @@ const UserSchema = new Schema({
     clerkId: { type: String, required: true, unique: true },
     email: { type: String, required: true, unique: true },
     username: { type: String, required: true, unique: true },
-    firstName: { type: String, required: true },
-    lastName: { type:String, required: true },
-    photo: { type: String, required: true },
+    firstName: { type: String },
+    lastName: { type:String },
+    photo: { type: String },
 })
+
+// firstName, lastName and photo are not required in case the user signs up
+// using only his email and username.
 
 const User = models.User || model('User', UserSchema);
 
